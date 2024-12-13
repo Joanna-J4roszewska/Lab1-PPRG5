a090e827e0bb4502e805a5a2276fafd38b7cc2c6
Joanna-J4roszewska <s34333@pjwstk.edu.pl>
2024-12-13 21:43:03 +0100

    Add files via upload

---
+++ 5.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Podaj liczbe elementow wektora: ";
+	cin >> n;
+
+	vector<double> wektor(n);
+
+	for (int i = 0; i < n; ++i) {
+		cout << "Podaj " << i + 1 << "-szy element wektora:";
+		cin >> wektor[i];
+	}
+	double max_wartosc = wektor[0];
+	int max_indeks = 0;
+
+	for (int i = 1; i < n; ++i) {
+		if (wektor[i] > max_wartosc) {
+
+			max_wartosc = wektor[i];
+			max_indeks = i;
+		}
+	}
+	cout << "Wartosc maksymalna: " << max_wartosc << endl;
+	cout << "Indeks elementu maksymalnego: " << max_indeks << endl;
+	return 0;
+}
---
+++ 5.2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+
+	cout << "Podaj rozmiar tablicy: ";
+	cin >> n;
+
+	if (n <= 0) {
+		cout << "Rozmiar tablicy musi byc wiekszy od zera.";
+		return 1;
+	}
+	int* tablica = new int[n];
+
+	cout << "Podaj " << n << " liczb calkowitych: ";
+	for (int i = 0; i < n; ++i) {
+		cin >> tablica[i];
+	}
+	cout << "Elementy tablicy to: ";
+	for (int i = 0; i < n; ++i) {
+		cout << tablica[i] << " ";
+	}
+
+	int left = 0, right = n - 1;
+	while (left < right) {
+		swap(tablica[left], tablica[right]);
+
+		left++;
+		right--;
+	}
+	cout << "Tablica po zamianie miejscami: ";
+	for (int i = 0; i < n; ++i) {
+		cout << tablica[i] << " ";
+
+	}
+	cout << endl;
+	return 0;
+}
